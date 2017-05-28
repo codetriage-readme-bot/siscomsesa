7cb708a5650554b591b105111748a9f474e4e0fd
Diego Biavati <diegobiavati@gmail.com>
2017-05-28 12:19:18 -0300

    Install locale pt-BR

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Siscomsesa
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # Use pt-BR in applicantion
+    config.i18n.default_locale = (:'pt-BR')
   end
 end
