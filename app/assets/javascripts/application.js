11833ae5506cdba8f0e0f53d05bda2b114c3de24
Diego Eduardo Ferreira Biavati <diegobiavati@gmail.com>
2017-06-06 13:10:26 +0000

    Change bootstrap for Materializecss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
 //= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
+//= require turbolinks
+//= require materialize-sprockets
 //= require_tree .
