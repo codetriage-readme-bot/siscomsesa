b533b44d7ea0396b27f49621e419accd55dce74b
Diego Biavati <diegobiavati@gmail.com>
2017-06-18 08:38:16 -0300

    install i18n in status

--- app/models/post_graduation.rb
+++ app/models/post_graduation.rb
@@ -2,4 +2,10 @@ class PostGraduation < ApplicationRecord
   enum status: [:active, :inactive]
 
   validates_presence_of :name, :initials, :seniority, :status
+
+  def self.status_attributes_for_select
+     statuses.map do |status, index|
+       [I18n.t("activerecord.attributes.#{model_name.i18n_key}.statuses.#{status}"), status]
+     end
+   end
 end
