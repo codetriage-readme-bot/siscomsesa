8cef9f8936d5402029adc9718a0209a69bfc7654
Diego Biavati <diegobiavati@gmail.com>
2017-06-19 19:43:47 -0300

    Revolv issue method not documentation

--- app/models/post_graduation.rb
+++ app/models/post_graduation.rb
@@ -3,9 +3,14 @@ class PostGraduation < ApplicationRecord
 
   validates_presence_of :name, :initials, :seniority, :status
 
+  # This method is used in select fields, and serves for an enum to be
+  # internationalized to the application language>
   def self.status_attributes_for_select
      statuses.map do |status, index|
-       [I18n.t("activerecord.attributes.#{model_name.i18n_key}.statuses.#{status}"), status]
+       [I18n.t(
+         "activerecord.attributes.#{model_name.i18n_key}.statuses.#{status}"),
+         status
+       ]
      end
    end
 end
