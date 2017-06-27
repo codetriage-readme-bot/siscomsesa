ed17cc05a878ed4d4ffea4ce1043146251e06764
Diego Biavati <diegobiavati@gmail.com>
2017-06-27 13:23:43 -0300

    Setup statuses in models (and i18n)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # This method is used in select fields, and serves for an enum to be
+  # internationalized to the application language>
+  def self.status_attributes_for_select
+    statuses.map do |status, _index|
+      [I18n.t(
+        "activerecord.attributes.#{model_name.i18n_key}.statuses.#{status}"
+      ),
+       status]
+    end
+  end
 end
